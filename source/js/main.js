226daf20963c99631d1bef748b58c686f281fcda
Alexander Komarov <komar81@gmail.com>
2018-10-24 16:46:24 +0300

    ДОбавил предзагрузку шрифтов, минификацию html + правки по критериям

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,13 @@
 //=require node_modules/picturefill/dist/picturefill.min.js
 
+var contactsMap = document.querySelector(".contacts__map--no-js");
+contactsMap.classList.remove("contacts__map--no-js");
+contactsMap.classList.add("contacts__map--js");
+
+var mainNav = document.querySelector(".main-nav--no-js");
+mainNav.classList.remove("main-nav--no-js");
+mainNav.classList.add("main-nav--js");
+
 function toggleMenu(e) {
   var topMenu = document.querySelector('.main-nav');
   e.preventDefault();
